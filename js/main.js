075f97204528909024c9bc21ebcde3bb513ea0ce
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-08-25 21:06:23 +0500

    add reklam

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let titleButtons = document.querySelector('.title__buttons')
+
+titleButtons.addEventListener('click', function (e) {
+  e.preventDefault();
+  window.open('https://lambent-semifreddo-2c1103.netlify.app/', '_blank')
+})
