d542b213603aee3b48baff392f61b67a3041e3fc
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-08-25 22:21:22 +0500

    add new function

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ titleButtons.addEventListener('click', function (e) {
   e.preventDefault();
   window.open('https://lambent-semifreddo-2c1103.netlify.app/', '_blank')
 })
+
+const container = document.querySelector('.containers');
+
+container.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.matches('.fa-instagram')) {
+    window.open('https://instagram.com/mv.nuriddin?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D', '_blank')
+  } else if (target.matches('.fa-github-square')) {
+    window.open('https://github.com/maxmutov-nuriddin/12-dars-imthon', '_blank')
+  } else if (target.matches('.fa-telegram')) {
+    window.open('https://t.me/mv_nuriddin', '_blank')
+  }
+});
